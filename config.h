62b6c75294a924b0dc3cc4b067182f16202422f6
isakhammer <isakhammer@gmail.com>
2020-12-11 10:37:28 +0100

    Added more stuff.

--- config.h
+++ config.h
@@ -4,8 +4,8 @@ static const Block blocks[] = {
 	/* {"⌨", "kbselect", 0, 30}, */
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	/* {"",	"music",	0,	11}, */
-	{"",	"pacpackages",	0,	8},
-	{"",	"news",		0,	6},
+	/* {"",	"pacpackages",	0,	8}, */
+	/* {"",	"news",		0,	6}, */
 	/* {"",	"crypto",	0,	13}, */
 	/* {"",	"price lbc \"LBRY Token\" 📚",			9000,	22}, */
 	/* {"",	"price bat \"Basic Attention Token\" 🦁",	9000,	20}, */
@@ -14,8 +14,8 @@ static const Block blocks[] = {
 	/* {"",	"price eth Ethereum 🍸",	9000,	23}, */
 	/* {"",	"price btc Bitcoin 💰",				9000,	21}, */
 	/* {"",	"torrent",	20,	7}, */
-	/* {"",	"memory",	10,	14}, */
-	/* {"",	"cpu",		10,	18}, */
+	{"",	"memory",	10,	14},
+	{"",	"cpu",		10,	18},
 	/* {"",	"moonphase",	18000,	17}, */
 	{"",	"forecast",	18000,	5},
 	{"",	"mailbox",	180,	12},
